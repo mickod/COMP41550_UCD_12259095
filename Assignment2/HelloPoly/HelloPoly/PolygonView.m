2efb574e8f1f6b48c08313743784ae1cb8d555b8
Mick <mick.odoherty@amodtech.com>
2013-01-16 23:07:44 +0000

    Main polygon screen drawing correctly

--- Assignment2/HelloPoly/HelloPoly/PolygonView.m
+++ Assignment2/HelloPoly/HelloPoly/PolygonView.m
@@ -10,6 +10,7 @@
 
 @interface PolygonView() 
     @property (nonatomic) NSArray* polygonPoints;
+    + (NSArray *)pointsForPolygonInRect:(CGRect)rect numberOfSides:(int)numberOfSides;
 @end
 
 @implementation PolygonView
@@ -23,19 +24,6 @@
     return self;
 }
 
-
-- (void)drawRect:(CGRect)rect
-{
-    //Get the number of sides from the controller using a protocol delegate mechanism
-    int numberOfSideFromDelegate = [self.polygonViewDelegate numberOfSidesForPolygonView];
-    NSLog(@"PolygonView - drawRect number of sides: %i", numberOfSideFromDelegate);
-    
-    //Generate the new polygon point array
-    
-    //draw the rectangle
-
-}
-
 + (NSArray *)pointsForPolygonInRect:(CGRect)rect numberOfSides:(int)numberOfSides {
     CGPoint center = CGPointMake(rect.size.width / 2.0, rect.size.height / 2.0);
     float radius = 0.9 * center.x; NSMutableArray *result = [NSMutableArray array];
@@ -47,8 +35,42 @@
         float curX = cos(newAngle) * radius;
         float curY = sin(newAngle) * radius;
         [result addObject:[NSValue valueWithCGPoint:
-                            CGPointMake(center.x+curX,center.y+curY)]];
+                           CGPointMake(center.x+curX,center.y+curY)]];
     }
     return result;
 }
+
+
+- (void)drawRect:(CGRect)rect
+{
+    //Get the number of sides from the controller using a protocol delegate mechanism
+    int numberOfSideFromDelegate = [self.polygonViewDelegate numberOfSidesForPolygonView];
+    NSLog(@"PolygonView - drawRect number of sides: %i", numberOfSideFromDelegate);
+    if (numberOfSideFromDelegate < 3) return;
+    
+    //Generate the new polygon point array
+    NSArray *polygonPoints = [PolygonView pointsForPolygonInRect:self.bounds
+                                                  numberOfSides:numberOfSideFromDelegate];
+    
+    //draw the rectangle, chceking firt that there are at least 3 points in case
+    //something went wrong getting the points
+    if ([polygonPoints count] < 3) return;
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    [[UIColor greenColor] setFill];
+    [[UIColor redColor] setStroke];
+    CGContextSetLineWidth(context, 2.0);
+    CGContextBeginPath(context);
+    CGPoint firstPoint = [polygonPoints[0] CGPointValue];
+    CGContextMoveToPoint(context, firstPoint.x, firstPoint.y);
+    for (id pointObject in polygonPoints) {
+        //get the CGPoint from the object
+        CGPoint nextPoint = [pointObject CGPointValue];
+        NSLog(@"PolygonView - drawRect point x %f", nextPoint.x);
+        NSLog(@"PolygonView - drawRect point y %f", nextPoint.y);
+        CGContextAddLineToPoint(context, nextPoint.x, nextPoint.y);
+    }
+    CGContextClosePath(context);
+    CGContextStrokePath(context);
+}
+
 @end
