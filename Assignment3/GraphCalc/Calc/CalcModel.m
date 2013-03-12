c23db8a523349206beefd64f32b548fdb56e4df9
Mick <mick.odoherty@amodtech.com>
2013-03-12 16:01:48 +0000

    Create new GraphCalc applictaion for Part 3

---
+++ Assignment3/GraphCalc/Calc/CalcModel.m
@@ -0,0 +1,199 @@
+//
+//  CalcModel.m
+//  Calc
+//
+//  Created by Mick O'Doherty on 12/02/2013.
+//  Copyright (c) 2013 Mick O'Doherty. All rights reserved.
+//
+
+#import "CalcModel.h"
+
+@interface CalcModel()
+@property (nonatomic, strong) NSMutableArray *expressionAsArray;
+@end
+
+@implementation CalcModel
+@synthesize operand = _operand;
+@synthesize waitingOperand = _waitingOperand;
+@synthesize waitingOperation = _waitingOperation;
+
+- init
+{
+    self = [super init];
+    if (!self) return nil;
+    
+    //Initialise the Expression mutable Array
+    self.expressionAsArray = [[NSMutableArray alloc] init];
+    
+    return self;
+}
+
+- (id) expression {
+    //Externally the expression is just an 'id' but
+    //internally we use an NSMutableArray. The getter for the
+    //externally visible expression property converts from
+    //the internal NSMutableArray to an id.
+    return self.expressionAsArray;
+}
+
+- (void) setUserEnteredOperand:(double)userEnteredOperand {
+    
+    //Add the operand to the model's expression
+    [self addItemToExpression: [NSNumber numberWithDouble:userEnteredOperand]];
+    
+    //Now set the operand
+    self.operand = userEnteredOperand;
+}
+
+- (void) setVariableAsOperand:(NSString *)variableName {
+   
+    //Add the operand to the model's expression
+    [self addItemToExpression: variableName];
+}
+
+- (void) addItemToExpression:(id)item {
+    
+    [self.expressionAsArray addObject:item];
+}
+
++ (NSString *) descriptionOfExpression:(id)anExpression {
+    
+    return [[anExpression valueForKey:@"description"] componentsJoinedByString:@""];
+}
+
+- (double)performOperation:(NSString *)operation {
+    
+    //Add the operation to the model's expression
+    [self addItemToExpression:operation];
+    
+    //Now perform the operation
+    if([operation isEqualToString:@"sqrt"]) {
+        if(self.operand >= 0) {
+            self.operand = sqrt(self.operand);
+        }else {
+            [self.calcModelDelegate receiveNotificationOfError:self :@"Cannot get sqrt of negative" ];
+            self.operand = 0;
+        }
+    } else if ([operation isEqualToString:@"+/-"]) {
+        self.operand = -self.operand;
+    } else if ([operation isEqualToString:@"Sin"]) {
+        if (self.useDegreesNotRads) {
+            //Convert the number to use degrees first
+            self.operand= self.operand * M_PI/180;
+        }
+        self.operand = sin(self.operand);
+    } else if ([operation isEqualToString:@"Cos"]) {
+        if (self.useDegreesNotRads) {
+            //Convert the number to use degrees first
+            self.operand= self.operand * M_PI/180;
+        }        
+        self.operand = cos(self.operand);
+    } else if ([operation isEqualToString:@"STO"]) {
+        self.memoryValue = self.operand;
+    } else if ([operation isEqualToString:@"RCL"]) {
+        self.operand = self.memoryValue;
+    } else if ([operation isEqualToString:@"M+"]) {
+        self.memoryValue = self.memoryValue + self.operand;
+    } else if ([operation isEqualToString:@"π"]) {
+        self.operand = M_PI;
+    } else if ([operation isEqualToString:@"C"]) {
+        self.memoryValue = 0;
+        self.operand = 0;
+        self.waitingOperation = Nil;
+        self.waitingOperand = 0;
+        [self.expressionAsArray removeAllObjects];
+    } else if ([operation isEqualToString:@"1/x"]) {
+        if (self.operand != 0) {
+            self.operand = 1/self.operand;
+        } else {
+            [self.calcModelDelegate receiveNotificationOfError:self :@"Cannot divide by 0" ];
+        }
+    } else {
+        [self performWaitingOperation];
+        self.waitingOperation = operation;
+        self.waitingOperand = self.operand;
+    }
+    
+    return self.operand;
+}
+
+- (void) performWaitingOperation {
+    if ([self.waitingOperation isEqualToString:@"+"])
+        self.operand = self.waitingOperand + self.operand;
+    else if ([self.waitingOperation isEqualToString:@"-"])
+        self.operand = self.waitingOperand - self.operand;
+    else if ([self.waitingOperation isEqualToString:@"*"])
+        self.operand = self.waitingOperand * self.operand;
+    else if ([self.waitingOperation isEqualToString:@"/"]) {
+        if (self.operand) {
+            self.operand = self.waitingOperand / self.operand;
+        } else {
+            [self.calcModelDelegate receiveNotificationOfError:self :@"Cannot divide by 0" ];
+        }
+    }
+}
+
++ (NSSet *) variablesInExpression:(id)anExpression {
+    
+    NSArray *expressionArray = anExpression;
+    NSMutableSet *variableMutableSet = [[NSMutableSet alloc] init];
+    
+    for (id expressionItem in expressionArray) {
+        //Check each string in the expression to see if it is one of our variables
+        if ([expressionItem isKindOfClass:[NSString class]]) {
+            //If the item is an operator then perform the operation
+            if ([expressionItem isEqualToString:@"x"] |
+                [expressionItem isEqualToString:@"a"] |
+                [expressionItem isEqualToString:@"b"] |
+                [expressionItem isEqualToString:@"c"]) {
+                
+                if ( ![variableMutableSet containsObject:expressionItem] ) {
+                    [variableMutableSet addObject:expressionItem];
+                }
+            }
+        }
+    }
+    
+    if ([variableMutableSet count] > 0) {
+        return variableMutableSet;
+    } else {
+        return nil;
+    }
+}
+
++ (double) evaluateExpression:(id)anExpression usingVariableValues:(NSDictionary *)variables {
+    
+    //Get the variables in the expression
+    NSSet *variablesInExpression = [CalcModel variablesInExpression:anExpression];
+    
+    //Now enumerate through expression again and evaluate it using
+    //an instance of this class CalcModel
+    NSArray *evaluationExpressionArray = anExpression;
+    CalcModel *tempCalcModel = [[CalcModel alloc] init];
+    double result = 0;
+    for (id expressionItem in evaluationExpressionArray) {
+        if ([expressionItem isKindOfClass:[NSNumber class] ]) {
+            //If the item is an operand simply set the operand
+           [tempCalcModel setUserEnteredOperand:[expressionItem doubleValue]];
+        } else if ([expressionItem isKindOfClass:[NSString class]]) {
+            //Check if the string is one of our variables
+            if ( [variablesInExpression containsObject:expressionItem]) {
+                //Switch the variable for its corresponding double value
+                double variableValue = [[variables objectForKey:expressionItem] doubleValue];
+                //Enter the operand
+                [tempCalcModel setUserEnteredOperand:variableValue];
+            } else {
+                //The string is an operator so perform the operation
+                result = [tempCalcModel performOperation:expressionItem];
+            }
+        } else {
+            result = 0;
+            break;
+        }
+    }
+    
+    //Return the result
+    return result;
+}
+
+@end
---
+++ Assignment3/GraphCalc/Calc/ViewController.m
@@ -0,0 +1,107 @@
+//
+//  ViewController.m
+//  Calc
+//
+//  Created by Mick O'Doherty on 12/02/2013.
+//  Copyright (c) 2013 Mick O'Doherty. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@end
+
+@implementation ViewController
+
+@synthesize calcModel = _calcModel;
+@synthesize calcDisplay = _calcDisplay;
+@synthesize isInTheMiddleOfTypingSomething = _isInTheMiddleOfTypingSomething;
+
+- (IBAction)variableButtonPressed:(UIButton *)sender {
+    
+    [self.calcModel setVariableAsOperand:sender.titleLabel.text];
+}
+
+- (IBAction)digitPressed:(UIButton *)sender {
+    NSString *digit = sender.titleLabel.text;
+    if (self.isInTheMiddleOfTypingSomething) {
+        //check to see if button pressed is a decimal point
+        if ([digit isEqualToString:@"."]) {
+            //If it is a decimal point check if there is one already - if so
+            //just return without doing anything else
+            if ([self.calcDisplay.text rangeOfString:@"."].location != NSNotFound) {
+                return;
+            }
+        }
+        self.calcDisplay.text = [self.calcDisplay.text stringByAppendingString:digit];
+    } else {
+        [self.calcDisplay setText:digit];
+        self.isInTheMiddleOfTypingSomething = YES;
+    }
+}
+- (IBAction)operationPressed:(UIButton *)sender {
+    if (self.isInTheMiddleOfTypingSomething) {
+        [self.calcModel setUserEnteredOperand:[self.calcDisplay.text doubleValue]];
+        self.isInTheMiddleOfTypingSomething = NO;
+    }
+    NSString *operation = sender.titleLabel.text;
+    double result = [self.calcModel performOperation:operation];
+    [self.calcDisplay setText:[NSString stringWithFormat:@"%g", result]];
+    [self.memoryDisplay setText:[NSString stringWithFormat:@"%g", self.calcModel.memoryValue]];
+    [self.expressionDisplay setText:[CalcModel descriptionOfExpression:self.calcModel.expression]];
+}
+
+- (IBAction)solveButtonPressed:(id)sender {
+    
+    //Clear the display first
+    [self.calcDisplay setText:@""];
+    
+    //Set dictionary values to test the evaluation
+    NSMutableDictionary *testVariables = [[NSMutableDictionary alloc] init];
+    [testVariables setValue:[NSNumber numberWithDouble:1] forKey:@"x"];
+    [testVariables setValue:[NSNumber numberWithDouble:2] forKey:@"a"];
+    [testVariables setValue:[NSNumber numberWithDouble:3] forKey:@"b"];
+    [testVariables setValue:[NSNumber numberWithDouble:4] forKey:@"c"];
+    
+    double evaluationResult = [CalcModel evaluateExpression:self.calcModel.expression usingVariableValues:testVariables];
+    
+    [self.calcDisplay setText:[NSString stringWithFormat:@"%g", evaluationResult]];
+}
+
+- (IBAction)degreeOrRadSelectionEvent:(UISegmentedControl*)sender {
+    NSString *segmentSelectedTitle = [sender titleForSegmentAtIndex:sender.selectedSegmentIndex] ;
+    
+    [self setCalcModelDegreeOrRadMode:segmentSelectedTitle];
+}
+
+- (void) receiveNotificationOfError:(CalcModel *)withErrorText :(NSString *)errorText {
+    UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                      message:errorText
+                                                     delegate:nil
+                                            cancelButtonTitle:@"OK"
+                                            otherButtonTitles:nil];
+    [message show];
+}
+
+- (void) viewDidLoad {
+    //Set this object as the delegate for its own calcModel
+    self.calcModel.calcModelDelegate = self;
+    //Set the degree or radians mode to the inital value of the segement selector
+    NSString *degreeOrRadSelected = [self.radianOrDegreesSegmentedController titleForSegmentAtIndex:self.radianOrDegreesSegmentedController.selectedSegmentIndex];
+    [self setCalcModelDegreeOrRadMode:degreeOrRadSelected];
+    [self.expressionDisplay setText:@""];
+}
+
+-(void) setCalcModelDegreeOrRadMode:(NSString*) selectionText {
+    
+    //If the selector is rad set the calc model to use radians, otherwise
+    //default to degrees (this covers the error case where the selected value is
+    //neither for some reason
+    if ( [selectionText isEqualToString:@"Rad"] ) {
+        self.calcModel.useDegreesNotRads = NO;
+    } else {
+        self.calcModel.useDegreesNotRads = YES;
+    }
+}
+
+@end
